73f92ef483dec1fa260816a5fca9b730f5c9197e
Manish938 <manish.mittal@metfic.co>
2023-08-30 14:00:00 +0530

    pagination for get all users api

--- admin/controllers/allUsers.js
+++ admin/controllers/allUsers.js
@@ -6,6 +6,7 @@ const { ResponseService } = require("../../common/responseService");
 module.exports.allUsers = async (req, res) => {
   try {
     const { token, adminid } = req.headers
+    const { pageNumber = 1, limit = 15 } = req.body
 
     if (!token) return ResponseService.failed(res, "token is required", StatusCode.badRequest)
     if (!adminid) return ResponseService.failed(res, "adminId is required", StatusCode.badRequest)
@@ -17,12 +18,22 @@ module.exports.allUsers = async (req, res) => {
 
     if (!admin) return ResponseService.failed(res, "unauthorized", StatusCode.unauthorized)
 
-    let users = await User.find();
+    const result = {};
+    const totalPosts = await User.countDocuments();
+    let startIndex = (pageNumber - 1) * limit;
+    result.totalPosts = totalPosts;
 
-    return ResponseService.success(res, "Users fetched successfully", users)
+    result.data = await User.find()
+      .sort("-createdAt")
+      .skip(startIndex)
+      .limit(limit);
+
+
+    return ResponseService.success(res, "Users fetched successfully", result)
 
   }
   catch (error) {
+    console.log(error)
     return ResponseService.failed(res, "Something went wrong", StatusCode.serverError)
   }
 };
